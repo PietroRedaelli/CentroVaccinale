1588f13005a813a5526aed28228971f045892868
PietroRedaelli <predaelli@studenti.uninsubria.it>
2021-09-18 16:14:43 +0200

    branch Pietro

---
+++ src/main/java/org/example/CittadinoLogin.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CittadinoLogin {
+    @FXML
+    Button login;
+    public void loginUtente(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/org/example/CittadinoMainMenu.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CittadinoMainMenu {
+    @FXML
+    Button log;
+    @FXML
+    Button registrazione;
+
+    public void registrati(ActionEvent actionEvent) {
+    }
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+}
